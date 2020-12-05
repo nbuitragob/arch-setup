ea00f9345cf922a71abda26a0ee18ba35dbcd908
nbuitragob <nicobuitra96@gmail.com>
2020-12-05 17:23:52 -0500

    Update and rename New Text Document.txt to init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ pacman -S --noconfirm xf86-video-amdgpu lightdm lightdm-gtk-greeter xorg-server 
 systemctl enable lightdm.service
 systemctl enable NetworkManager.service
 cp lightdm.conf /etc/lightdm/lightdm.conf
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
