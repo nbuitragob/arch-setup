018759dabc3b4b43705de66631bbb20afbb8c581
nbuitragob <nicobuitra96@gmail.com>
2020-12-05 17:28:00 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
+cp locale.gen /etc/locale.gen
+locale-gen
+
+cp hostname /etc/hostname
+cp hosts /etc/hosts
+
 pacman -S --noconfirm xf86-video-amdgpu lightdm lightdm-gtk-greeter xorg-server gvim i3 lxterminal networkmanager nm-applet
 systemctl enable lightdm.service
 systemctl enable NetworkManager.service
 cp lightdm.conf /etc/lightdm/lightdm.conf
-ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
-hwclock --systohc
+
+
