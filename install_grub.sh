992c31d352757af3ede54ef18921b1bc293b67f0
nbuitragob <nicobuitra96@gmail.com>
2020-12-05 20:05:38 -0500

    Update install_grub.sh

--- install_grub.sh
+++ install_grub.sh
@@ -30,4 +30,6 @@ mount /dev/sda1 /boot/EFI
 grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
+mkdir /home/melferas
+
 pacman -S --needed --noconfirm i3
