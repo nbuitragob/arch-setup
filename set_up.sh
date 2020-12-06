f1cef8ab27178c9a79e3a6ea563a71fe14271cc6
Melferas <nbuitragob@unal.edu.co>
2020-12-06 14:44:51 -0500

    added dmenu and feh

--- set_up.sh
+++ set_up.sh
@@ -10,4 +10,4 @@ scripts/install_grub.sh "$1"
 mkdir -p /etc/X11/xorg.conf.d/
 cp config/lightdm/20-keyboard.conf /etc/X11/xorg.conf.d/
 cp -r dotfiles/.config /home/melferas/
-
+chowm -R /home/melferas
