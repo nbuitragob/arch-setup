c3ed9ee8efaa04de73810b50e61ec574e4e54256
nbuitragob <nicobuitra96@gmail.com>
2020-12-07 08:39:27 -0500

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -101,6 +101,7 @@ parted -s -- $HD mkpart primary $HOME_FS $HOME_START -0 1>/dev/null
 
 # Formats the root, home and boot partition to the specified file system
 echo "Formating boot partition"
+echo "mkfs.$BOOT_FS ${HD}1 -L Boot 1>/dev/null"
 mkfs.$BOOT_FS ${HD}1 -L Boot 1>/dev/null
 echo "Formating root partition"
 mkfs.$ROOT_FS ${HD}3 -L Root 1>/dev/null
