6488e84663215c118080fd8bf0038ac2a04a809d
nbuitragob <nicobuitra96@gmail.com>
2020-12-07 12:55:50 -0500

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -48,64 +48,6 @@ SWAP_SIZE=+4G
 HOME_FS=ext4
 ROOT_FS=ext4
 
-# Extra packages (not obligatory)
-EXTRA_PKGS='vim'
-
-
-#!/bin/bash
-# encoding: utf-8
-
-##################################################
-#		    Variables 			 #
-##################################################
-# Computer Name
-HOSTN=void-pc
-
-# Keyboard Layout
-KEYBOARD_LAYOUT=la-latin1
-
-# Your language, used for localization purposes
-LANGUAGE=en_US
-
-# Geography Localization. Verify the directory /usr/share/zoneinfo/<Zone>/<SubZone>
-LOCALE=America/Bogota
-
-# Admin username for the brand new installed system
-read -p 'Admin username: ' USERNAME
-
-# Admin's and root's password for the brand new installed system
-read -sp 'Password: ' PASSWORD
-echo
-read -sp 'Verify password: ' PASSWORD_2
-if [ "$PASSWORD" = "$PASSWORD_2" ]; then
-       echo -e "\npasswords match"
-else 
-       echo "passwords don't match"
-       exit 1
-fi
-
-# Root password for the brand new installed system
-ROOT_PASSWD=$PASSWORD
-
-########## Hard Disk Partitioning Variable
-# ANTENTION, this script erases ALL YOU HD DATA (specified bt $HD)
-read -p 'Filesystem e.g /dev/sda: ' HD
-# Boot Partition Size: /boot
-BOOT_SIZE=+1G
-# Root Partition Size: /
-ROOT_SIZE=+100G
-# Swap partition size: /swap
-SWAP_SIZE=+4G
-# The /home partition will occupy the remain free space
-
-# Partitions file system
-HOME_FS=ext4
-ROOT_FS=ext4
-
-# Extra packages (not obligatory)
-EXTRA_PKGS='vim'
-
-
 ##################################################
 #		    Script 			 #
 ##################################################
